ec674188085e8e293463c9238d2436d9d5db3675
Masum Sakib <30460180+masumskaib396@users.noreply.github.com>
2018-09-02 01:36:06 -0700

    university

---
+++ university/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){     
+  
+      /*=============================
+          magnificPopup-js
+        ==============================*/
+
+        
+        $(".vide-btn").magnificPopup({
+    			type:'video'
+    		});
+		
+      
+       
+    });
+
+
+
+
+}(jQuery));
